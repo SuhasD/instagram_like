6da62aebdf569fdc96ca07d2cf9ac1d377b60a07
vadim <vadim@vadims-MacBook-Pro.local>
2018-07-02 17:55:28 +0300

    begin to create pseudo-backend with main page

--- build/components/main-page/index.js
+++ build/components/main-page/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import incoming from '../../../backend/incoming.json';
 class AppNavigator extends Component {
+    constructor() {
+        super(...arguments);
+        this.addNewPhoto = () => {
+            console.log('new Photo here...');
+        };
+    }
     render() {
+        console.log('incoming', incoming);
         return (React.createElement(View, { style: styles.container },
-            React.createElement(Text, null, "Main")));
+            incoming.user.imgs.map(item => (React.createElement(View, { key: item.name },
+                React.createElement(Image, { source: { uri: item.src }, style: {
+                        width: 100,
+                        height: 100
+                    } }),
+                React.createElement(Text, null, item.name)))),
+            React.createElement(Text, null, "Main"),
+            React.createElement(TouchableOpacity, { onPress: this.addNewPhoto, style: styles.touchContainer },
+                React.createElement(Text, { style: styles.iconView },
+                    React.createElement(FontAwesome, { name: 'plus-circle', size: 60 })),
+                ";")));
     }
 }
 export default AppNavigator;
@@ -13,6 +32,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    touchContainer: {
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+        backgroundColor: 'transparent'
+    },
+    iconView: {
+        margin: 10,
+        color: 'blue'
     }
 });
 //# sourceMappingURL=index.js.map
--- src/components/main-page/index.tsx
+++ src/components/main-page/index.tsx
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Image
 } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import incoming from '../../../backend/incoming.json';
 
 interface IInputState {
 }
@@ -13,10 +18,39 @@ interface IInputProps {
 
 class AppNavigator extends Component<IInputProps, IInputState> {
 
+  addNewPhoto = () => {
+    console.log('new Photo here...');
+  }
+
   render() {
+    console.log('incoming', incoming);
     return (
       <View style = { styles.container } >
+        {
+          incoming.user.imgs.map(item => (
+            <View key={item.name}>
+              <Image
+                source={{uri: item.src}}
+                style={{
+                  width: 100,
+                  height: 100
+                }} />
+                <Text>{item.name}</Text>
+            </View>
+          ))
+        }
          <Text>Main</Text>
+
+         <TouchableOpacity
+           onPress={this.addNewPhoto}
+           style={styles.touchContainer}>
+           <Text style={styles.iconView}>
+             <FontAwesome
+               name={'plus-circle'}
+               size={60} />
+           </Text>;
+         </TouchableOpacity>
+
       </View>
     );
   }
@@ -31,5 +65,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  touchContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    backgroundColor: 'transparent'
+  },
+  iconView: {
+    margin: 10,
+    color: 'blue'
   }
 });
