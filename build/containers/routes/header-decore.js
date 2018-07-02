0add62dde257070aa0ab9d04134bc4d4345cdd4e
vadim <vadim@vadims-MacBook-Pro.local>
2018-07-02 16:30:42 +0300

    create simple routing with empty components

---
+++ build/containers/routes/header-decore.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+const LoginLogo = ({ onPress, goBack }) => {
+    return (React.createElement(View, { style: styles.container },
+        !onPress
+            ? React.createElement(TouchableOpacity, { onPress: goBack, style: styles.touchContainer },
+                React.createElement(Text, { style: styles.iconView },
+                    React.createElement(FontAwesome, { name: 'arrow-left', size: 20 })),
+                ";")
+            : React.createElement(View, { style: styles.emptyContainer }),
+        React.createElement(Text, { style: styles.mainLogo }, "Instagram Like..."),
+        onPress
+            ? React.createElement(TouchableOpacity, { onPress: onPress, style: styles.touchContainer },
+                React.createElement(Text, { style: styles.iconView },
+                    React.createElement(FontAwesome, { name: 'user-circle', size: 30 })),
+                ";")
+            : React.createElement(View, { style: styles.emptyContainer })));
+};
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        height: 60,
+        backgroundColor: 'pink',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    touchContainer: {
+        backgroundColor: 'transparent'
+    },
+    iconView: {
+        margin: 10,
+        color: 'black'
+    },
+    emptyContainer: {
+        flex: 0.15
+    },
+    mainLogo: {
+        flex: 0.7,
+        textAlign: 'center'
+    }
+});
+export default LoginLogo;
+//# sourceMappingURL=header-decore.js.map
---
+++ build/containers/routes/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import MainPage from '../../components/main-page';
+import ProfilePage from '../../components/profile-page';
+import LoginLogo from './header-decore';
+const AppNavigator = ({}) => {
+    return React.createElement(OnBoarding, null);
+};
+export default AppNavigator;
+export const OnBoarding = createStackNavigator({
+    main: {
+        screen: MainPage,
+        navigationOptions: ({ navigation }) => (Object.assign({ header: React.createElement(LoginLogo, { onPress: () => navigation.navigate('profile') }) }, headerView))
+    },
+    profile: {
+        screen: ProfilePage,
+        navigationOptions: ({ navigation }) => (Object.assign({ header: React.createElement(LoginLogo, { goBack: () => navigation.navigate('main') }) }, headerView))
+    }
+});
+const styles = StyleSheet.create({
+    headerTitleStyle: {
+        flex: 6,
+        padding: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    headerStyle: {
+        marginTop: 20,
+        backgroundColor: '#ffffff',
+        shadowColor: '#ffffff'
+    }
+});
+const headerView = {
+    headerTitleStyle: styles.headerTitleStyle,
+    headerStyle: styles.headerStyle
+};
+//# sourceMappingURL=index.js.map
